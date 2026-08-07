a1b84ab8f914c3bacf94f8682859e2d1bc170b3a
DMCstiven1 <stivenmontero008@gmail.com>
2026-08-06 21:56:16 -0400

    Add Discord OAuth2 sign-in redirect function

--- functions/auth/signin.js
+++ functions/auth/signin.js
@@ -1,1 +1,12 @@
+export async function onRequest(context) {
+  const { env } = context;
 
+  const discord =
+    "https://discord.com/api/oauth2/authorize" +
+    `?client_id=${env.DISCORD_CLIENT_ID}` +
+    "&response_type=code" +
+    `&redirect_uri=${encodeURIComponent(env.DISCORD_REDIRECT_URI)}` +
+    "&scope=identify guilds";
+
+  return Response.redirect(discord, 302);
+}
