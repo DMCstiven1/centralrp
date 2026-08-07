56fe23bc8270cd1a5ba1eaf08b1c69a46602d9cc
DMCstiven1 <stivenmontero008@gmail.com>
2026-08-07 03:17:46 -0400

    Update favicon link in callback.js

--- functions/callback.js
+++ functions/callback.js
@@ -62,7 +62,7 @@ export async function onRequest(context) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Central RP</title>
-    <link rel="icon" type="image/png" href="https://i.imgur.com/rg130IN.png">
+    <link rel="icon" type="image/png" href="https://i.imgur.com/yiFi8Ke.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Outfit:wght@800;900&display=swap" rel="stylesheet">
