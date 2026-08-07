f04e45e9ca2ea3ad1ff65edeb1d73a3ccd585d7b
DMCstiven1 <stivenmontero008@gmail.com>
2026-08-07 04:18:59 -0400

    Update me.js

--- api/me.js
+++ api/me.js
@@ -1,1 +1,29 @@
+export default function handler(req, res) {
+    const cookies = req.headers.cookie || "";
 
+    const match = cookies.match(
+        /(?:^|;\s*)discord_user=([^;]+)/
+    );
+
+    if (!match) {
+        return res.status(401).json({
+            loggedIn: false
+        });
+    }
+
+    try {
+        const user = JSON.parse(
+            decodeURIComponent(match[1])
+        );
+
+        return res.status(200).json({
+            loggedIn: true,
+            user
+        });
+
+    } catch (error) {
+        return res.status(401).json({
+            loggedIn: false
+        });
+    }
+}
