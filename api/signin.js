d8b094a0ccb5e48f084ecf2086885dc886788a60
DMCstiven1 <stivenmontero008@gmail.com>
2026-08-07 03:59:36 -0400

    Add Discord OAuth2 sign-in handler

--- api/signin.js
+++ api/signin.js
@@ -1,1 +1,13 @@
+export default function handler(req, res) {
+    const clientId = process.env.DISCORD_CLIENT_ID;
+    const redirectUri = process.env.DISCORD_REDIRECT_URI;
 
+    const discordUrl =
+        "https://discord.com/api/oauth2/authorize" +
+        `?client_id=${clientId}` +
+        "&response_type=code" +
+        `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+        "&scope=identify%20guilds";
+
+    res.redirect(302, discordUrl);
+}
